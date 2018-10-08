409bc513e5a3fcdcd317427b456ea2d100c00bc4
NielsVerheuen <niels.verheuen@gmail.com>
2018-10-08 17:57:37 +0200

    Added password toggle CSS + JS

--- script/source.js
+++ script/source.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     console.log('Script loaded!');
-
+    handleFloatingLabel();
 });
+
+function handleFloatingLabel() {
+
+    let input = document.querySelector(".js-name-input");
+    input.addEventListener("blur", handleInputListener)
+}
+
+function handleInputListener(e) {
+    let label = document.querySelector(".js-floating-label");
+    console.log("hier papier:", this.value);
+    if (this.value != "") {
+        label.classList.add("is-floating");
+    } else {
+        label.classList.remove("is-floating");
+    }
+}
+
+function togglePassword() {
+    var passwordInput = document.querySelector(".js-password-input");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
